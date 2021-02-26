0cb0da07df70fe5df14ce2f556cc57dfbfa27110
gouvina <germanouvina@gmail.com>
2021-02-26 16:43:54 -0300

    Base structure created

--- app.js
+++ app.js
@@ -1,51 +1,20 @@
-// Global Environment Variables
-require('dotenv').config();
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
-// Library dependencies
-const express = require('express');
-const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
-const logger = require('morgan');
-const cors = require('cors');
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 
-// Local dependencies
-const routes = require('./constants/routes');
-const usersRouter = require('./routes/users');
-const { CORS_ORIGIN_WHITELIST } = require('./config');
+var app = express();
 
-// CORS options
-const corsOptions = {
-  origin(origin, callback) {
-    if (CORS_ORIGIN_WHITELIST.indexOf(origin) !== -1 || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  credentials: true,
-};
-
-// Express app rules
-const app = express();
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(cors(corsOptions));
+app.use(express.static(path.join(__dirname, 'public')));
 
-// Routes
-app.use(routes.USERS, usersRouter);
-
-// Error handling
-app.use(( err, req, res, next ) => {
-  res.locals.error = err;
-  if (err.status >= 100 && err.status < 600)
-    res.status(err.status);
-  else
-    res.status(500);
-  res.render('error');
-});
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 
 module.exports = app;
