36fe2a263c580f0bedf2b315d21def19d544befe
Germán Ouviña <germanouvina@gmail.com>
2021-07-04 21:59:27 -0300

    Small changes

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ require('dotenv').config();
 
 // Global dependencies
 const express = require('express');
-const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const cors = require('cors');
